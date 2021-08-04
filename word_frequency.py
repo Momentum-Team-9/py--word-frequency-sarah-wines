6cbb8c067059fec1799f42fc2440e6cce3596d73
sarah-wines <sarah.lodico.wines@gmail.com>
2021-08-04 18:15:35 -0400

    clean text, stringified

--- word_frequency.py
+++ word_frequency.py
@@ -1,13 +1,21 @@
 STOP_WORDS = [
-    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
-    'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
-    'will', 'with'
-]
+    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has',
+    'he', 'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to',
+    'were', 'will', 'with']
 
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    import string
+
+    with open(file, "r") as file_text:
+        text = file_text.read()
+        text = text.lower()
+        words = text.split()
+        table = str.maketrans("", "", string.punctuation)
+        stripped = [w.translate(table) for w in words]
+        print(stripped)
+        file_text.closed
 
 
 if __name__ == "__main__":
